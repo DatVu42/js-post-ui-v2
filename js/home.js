fc1ab5f2b5d24564eea1085dcfed6b5da3b2d4b5
Vu Dat <vudat42@gmail.com>
2024-03-31 10:06:54 +0700

    set default params

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 import postApi from './api/postApi'
-import { setTextContent, truncateText } from './utils'
+import {getUlPagination, setTextContent, truncateText} from './utils'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
@@ -18,14 +18,22 @@ function createPostElement(post) {
     thumbnailElement.src = post.imageUrl
 
     thumbnailElement.addEventListener('error', () => {
-        thumbnailElement.src = 'https://placehold.co/1368x400?text=Thumbnail'
+      thumbnailElement.src = 'https://placehold.co/1368x400?text=Thumbnail'
     })
   }
 
   setTextContent(liElement, '[data-id="title"]', post.title)
-  setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 100))
+  setTextContent(
+    liElement,
+    '[data-id="description"]',
+    truncateText(post.description, 100),
+  )
   setTextContent(liElement, '[data-id="author"]', post.author)
-  setTextContent(liElement, '[data-id="timeSpan"]', ` - ${dayjs(post.updatedAt).fromNow()}`)
+  setTextContent(
+    liElement,
+    '[data-id="timeSpan"]',
+    ` - ${dayjs(post.updatedAt).fromNow()}`,
+  )
 
   return liElement
 }
@@ -41,11 +49,49 @@ function renderPostList(postList) {
   })
 }
 
+function handlePrevClick() {
+  console.log('Prev click')
+}
+
+function handleNextClick() {
+  console.log('Next click')
+}
+
+function handlePagination() {
+  const ulPagination = getUlPagination()
+  if (!ulPagination) return
+
+  const prevLink = ulPagination.firstElementChild?.firstElementChild
+  if (prevLink)
+    prevLink.addEventListener('click', (e) => {
+      e.preventDefault()
+      handlePrevClick()
+    })
+
+  const nextLink = ulPagination.lastElementChild?.firstElementChild
+  if (nextLink)
+    nextLink.addEventListener('click', (e) => {
+      e.preventDefault()
+      handleNextClick()
+    })
+}
+
+function initUrl() {
+  const url = new URL(window.location)
+  if (!url.searchParams.get('_page')) url.searchParams.set('_page', 1)
+  if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6)
+  history.pushState({}, '', url)
+}
+
 ;(async () => {
-  const queryParams = {
-    _page: 1,
-    _limit: 6,
+  try {
+    initUrl()
+
+    const queryParams = new URLSearchParams(window.location.search)
+    const {data, pagination} = await postApi.getAll(queryParams)
+    renderPostList(data)
+    handlePagination()
+  } catch (error) {
+    console.log('Network something wrong!', error)
   }
-  const {data, pagination} = await postApi.getAll(queryParams)
-  renderPostList(data)
 })()
